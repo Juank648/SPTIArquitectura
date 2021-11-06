f4d1598ea2fe61829e1fb4ceeb21ad1ae5801e4b
Juan Alayon <jkam86425@gmail.com>
2021-11-05 19:22:33 -0500

    Formas y errores corregidos

---
+++ src/main/java/com/arq/sptiarquitectura/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.arq.sptiarquitectura.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/userView")
+    public String userForm(){
+        return "user-form/user-view";
+    }
+
+}
