5ec0b2d0bb07baf518ac2e3e73bae0087ca638e5
Juan Alayon <jkam86425@gmail.com>
2021-11-12 14:37:15 -0500

    App finished

--- src/main/java/com/arq/sptiarquitectura/entity/User.java
+++ src/main/java/com/arq/sptiarquitectura/entity/User.java
@@ -3,23 +3,15 @@ package com.arq.sptiarquitectura.entity;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
 
-@Entity 
+@Entity
+@Table(name="UserSPTI")
 public class User implements Serializable{
 
 	/**
