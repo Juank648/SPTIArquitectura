f0df495bb05c27338fea73b8a9616273c7f5e888
Juan Alayon <jkam86425@gmail.com>
2021-11-12 19:04:49 -0500

    Update SptiArquitecturaApplication.java

--- src/main/java/com/arq/sptiarquitectura/SptiArquitecturaApplication.java
+++ src/main/java/com/arq/sptiarquitectura/SptiArquitecturaApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SptiArquitecturaApplication {
 
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SptiArquitecturaApplication.class, args);
-
 	}
 
 }
