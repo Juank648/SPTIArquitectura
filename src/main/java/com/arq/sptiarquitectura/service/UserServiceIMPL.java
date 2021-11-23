29fb6a9d87bdca2167bd1c5008536ab0e877a6f0
Laura <lrgr000@gmail.com>
2021-11-23 12:13:20 -0500

    CambioMayusculaUserServiceIMPL

--- src/main/java/com/arq/sptiarquitectura/service/UserServiceIMPL.java
+++ src/main/java/com/arq/sptiarquitectura/service/UserServiceIMPL.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceIMPL implements UserService{
 
 	@Autowired
 	UserRepository repository;
